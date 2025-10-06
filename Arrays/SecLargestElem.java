c9c1096a6cabb90e4127f65d522ea39414ad8306
khushboo-khatoon <khushboo071006@gmail.com>
2025-10-06 19:37:09 +0530

    added the solution of second largest element in the array

---
+++ Arrays/SecLargestElem.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class SecLargestElem {
+
+   public int getSecondLargest(int[] arr) {
+        // code here
+        int max=arr[0];
+        int secmax=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            max=Math.max(max,arr[i]);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>secmax && arr[i]!=max){
+                secmax=arr[i];
+            }
+        }
+        if(secmax==Integer.MIN_VALUE){
+            return -1;
+        }
+        return secmax;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 7, 3, 2, 2, 4};
+        SecLargestElem obj = new SecLargestElem();
+        int secondLargest = obj.getSecondLargest(array);
+      System.out.println("The second largest element in the array is: " + secondLargest);
+    }
+    
+}
