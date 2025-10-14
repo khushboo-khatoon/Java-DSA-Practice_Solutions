70a8b2c8681b7e9c364751bb19fdac59f5620b54
khushboo-khatoon <khushboo071006@gmail.com>
2025-10-15 04:36:10 +0530

    Added max_min_Elem.java to Arrays folder

---
+++ Arrays/max_min_Elem.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class max_min_Elem {
+      public ArrayList<Integer> getMinMax(int[] arr) {
+        // code Here
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        
+        ArrayList<Integer> res=new ArrayList<>();
+  
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        res.add(min);
+        res.add(max);
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {3, 5, 1, 8, -2, 7};
+        max_min_Elem obj = new max_min_Elem();
+        ArrayList<Integer> result = obj.getMinMax(array);
+        System.out.println("The minimum element in the array is: " + result.get(0));
+        System.out.println("The maximum element in the array is: " + result.get(1));
+    }
+}
+
+
+
