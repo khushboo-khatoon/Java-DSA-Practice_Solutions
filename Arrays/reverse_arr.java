0b5681669534c4c82123d919eca93ba587b1cf9a
khushboo-khatoon <khushboo071006@gmail.com>
2025-10-18 19:21:53 +0530

    Added Reverse Array program

---
+++ Arrays/reverse_arr.java
@@ -0,0 +1,20 @@
+package Arrays;
+public class reverse_arr {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        reverseArray(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void reverseArray(int[] arr) {
+         int n=arr.length;
+        int[] temp=new int[n];
+       for(int i=0;i<n/2;i++){
+           temp[i]=arr[i];
+           arr[i]=arr[n-1-i];
+           arr[n-i-1]=temp[i];
+       }
+    }
+}
