7cebe09a1ecd1a53efa2fd3733e0b1d8739eb17b
khushboo-khatoon <khushboo071006@gmail.com>
2025-10-19 22:55:53 +0530

    aadded right rotation of array elemnts

---
+++ Arrays/right_rotate.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class right_rotate {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int d = 2; //position
+        rightRotate(arr, d);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void rightRotate(int[] arr, int d) {
+        int n = arr.length;
+        d = d % n; // if d>n
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, d - 1);
+        reverse(arr, d  , n - 1);
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    
+}
