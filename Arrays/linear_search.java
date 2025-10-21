2237bdb4874928c63f92a6bd2370b77b71769173
khushboo-khatoon <khushboo071006@gmail.com>
2025-10-21 17:19:26 +0530

    linear search

---
+++ Arrays/linear_search.java
@@ -0,0 +1,28 @@
+// search an element in array | Time Complexity: O(N) | Space Complexity: O(1)
+
+package Arrays;
+
+public class linear_search {
+    public int search(int arr[], int x) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] array = {4, 2, 3, 5, 1};
+        int target = 5;
+        linear_search obj = new linear_search();
+        int result = obj.search(array, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
+    
+
