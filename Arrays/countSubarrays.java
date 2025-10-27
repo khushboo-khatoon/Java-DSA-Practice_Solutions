19b23ffdd2f1c3ffd2126c377a6a35c6667fd1f3
khushboo-khatoon <khushboo071006@gmail.com>
2025-10-27 05:57:28 +0530

    subarrays where the maximum element is greater than k

---
+++ Arrays/countSubarrays.java
@@ -0,0 +1,25 @@
+public class countSubarrays {
+      long countSubarray(int arr[], int n, int k) {
+
+        // code here
+        long pos=0,res=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>k){
+                pos=i+1;
+            }
+            res+=pos;
+        }
+        return res;
+        
+    }
+    public static void main(String[] args) {
+        countSubarrays cs=new countSubarrays();
+        int arr[]={1,2,3,4};
+        int n=arr.length;
+        int k=1;
+        long result=cs.countSubarray(arr,n,k);
+        System.out.println("The count of subarrays is: "+result);
+    }
+
+    
+}
