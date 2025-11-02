d9e69398f372198d73011cf18a744ad839d28d67
khushboo-khatoon <khushboo071006@gmail.com>
2025-11-02 20:33:18 +0530

    Two Sum - Pair with Given Sum

---
+++ Arrays/PairSum.java
@@ -0,0 +1,34 @@
+// Find if there exists a pair with sum equal to given target in an array | Time Complexity: O(N log N) | Space Complexity: O(1)
+
+package Arrays;
+
+import java.util.Arrays;
+
+public class PairSum {
+       boolean twoSum(int arr[], int target) {
+            Arrays.sort(arr);
+        int i = 0, j= arr.length - 1;
+        while (i <j) {
+            int sum = arr[i] + arr[j];
+
+            if (sum == target)
+                return true;
+            else if (sum < target)
+            
+                i++; 
+            else
+            
+                j--;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        PairSum ps = new PairSum();
+        int[] arr = {10, 15, 2, 7};
+        int target = 17;
+        boolean result = ps.twoSum(arr, target);
+        System.out.println("Pair with the given sum " + target + " exists: " + result);
+    }
+
+    
+}
