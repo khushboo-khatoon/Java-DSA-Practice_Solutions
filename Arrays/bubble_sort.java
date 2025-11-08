40b3cee9ef13557f0519afe8e92b3cfb9363e730
khushboo-khatoon <khushboo071006@gmail.com>
2025-11-08 23:36:57 +0530

    Bubble sort

---
+++ Arrays/bubble_sort.java
@@ -0,0 +1,25 @@
+
+public class bubble_sort {
+       public void bubbleSort(int[] arr) {
+        // code here
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                }
+                
+            }
+        }
+    }
+    public static void main(String[] args) {
+        bubble_sort bs=new bubble_sort();
+        int arr[]={5,4,3,2,1};
+        bs.bubbleSort(arr);
+        System.out.println("The sorted array is: ");
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
