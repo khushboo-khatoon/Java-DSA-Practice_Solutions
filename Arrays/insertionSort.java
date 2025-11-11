efc8d00050bdc5914a04e42317bf5026880b2a08
khushboo-khatoon <khushboo071006@gmail.com>
2025-11-11 23:33:14 +0530

    insertion sort

---
+++ Arrays/insertionSort.java
@@ -0,0 +1,25 @@
+
+public class insertionSort {
+     public void insertion(int arr[]) {
+        // code here
+        for(int i=1;i<arr.length;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j]<arr[j-1]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        insertionSort is=new insertionSort();
+        int arr[]={5,4,3,2,1};
+        is.insertion(arr);
+        System.out.println("The sorted array is: ");
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+
+}
