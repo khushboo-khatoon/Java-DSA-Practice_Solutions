57a173b265477ec66cd8a3447b318e1de2cbc551
khushboo-khatoon <khushboo071006@gmail.com>
2026-01-06 23:53:58 +0530

    the mejority element greater then n/2

---
+++ Arrays/mejority_element.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class mejority_element {
+    public static int majorityElement(int[] nums) {
+                int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        for(int k:nums){
+            map.put(k,map.getOrDefault(k,0)+1);
+        }
+        
+        for(Map.Entry<Integer,Integer> e:map.entrySet()){
+            if(e.getValue()>n/2){
+                return e.getKey();
+                
+            }
+        }
+        
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,2,2,2};
+        int ans=majorityElement(arr);
+        System.out.println("the mazority element greater then n/2 is "+ans );
+    }
+    
+}
