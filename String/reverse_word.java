77f46830c3e2aedf9b9dde9e941e6a6c9b36dee4
khushboo-khatoon <khushboo071006@gmail.com>
2026-03-30 11:16:11 +0530

    revers every word in a String

---
+++ String/reverse_word.java
@@ -0,0 +1,24 @@
+
+
+public class reverse_word {
+        public String reverseWords(String s) {
+        
+        StringBuilder sb=new StringBuilder();
+        String[] words=s.split(" +");
+        int i=words.length-1;
+        while(i>=0){
+           sb.append(words[i]);
+           sb.append(" ");
+           i--;
+        }
+
+        return sb.toString().trim();
+    }
+    public static void main(String[] args) {
+        reverse_word obj = new reverse_word();
+        String s = "Hello World";
+        String result = obj.reverseWords(s);
+        System.out.println("Reversed words: " + result); // Output: "World Hello"
+    }
+    
+}
