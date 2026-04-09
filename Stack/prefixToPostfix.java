8d35c46e9c0c6a8d0f1c7a2afd0fb9144b296dca
khushboo-khatoon <khushboo071006@gmail.com>
2026-04-09 23:52:52 +0530

    Prefix to Postfix Conversion

--- Stack/prefixToPostfix.java
+++ Stack/prefixToPostfix.java
@@ -1,10 +1,11 @@
 package Stack;
+
 import java.util.Stack;
 
-public class prefixToInfix {
-    static String preToInfix(String pre_exp) {
+public class prefixToPostfix {
+    static String preToPost(String pre_exp) {
         // code here
-         Stack<String> st=new Stack<>();
+          Stack<String> st=new Stack<>();
        
         int i=pre_exp.length()-1;
         while(i>=0){
@@ -20,7 +21,7 @@ public class prefixToInfix {
                 st.pop();
                 String t2=st.peek();
                 st.pop();
-               String concat='('+t1+ch+t2+')' ;
+               String concat=t1+t2+ch;
                st.push(concat);
             }  
             
@@ -28,11 +29,9 @@ public class prefixToInfix {
             
         }
         return st.peek();
-    }    public static void main(String[] args) {
-        String pre_exp="*+AB-CD";
-        System.out.println(preToInfix(pre_exp));
     }
-
-
-    
+    public static void main(String[] args) {
+        String pre_exp="*+AB-CD";
+        System.out.println(preToPost(pre_exp));
+    } 
 }
